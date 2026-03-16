0b1b6fe6f517783226236d939bf4aee67a950ce2
Marius <marius.meyer.it@gmail.com>
2026-03-16 13:37:26 +0100

    move steop

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,13 @@
 import amqp from "amqplib";
 import { clientWelcome,commandStatus,getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/declareAndBind.js";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { ArmyMovesPrefix, ExchangePerilDirect, ExchangePerilTopic, PauseKey } from "../internal/routing/routing.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
-import { commandMove } from "../internal/gamelogic/move.js";
+import { commandMove, handleMove } from "../internal/gamelogic/move.js";
 import { subscribeJSON } from "../internal/pubsub/subscribeJSON.js";
-import { handlerPause } from "./handlers.js";
+import { handlerPause, handlerMove } from "./handlers.js";
+import { publishJSON } from "../internal/pubsub/publishJSON.js";
 async function main() {
   console.log("Starting Peril client...");
 
@@ -17,18 +18,14 @@ async function main() {
   let userName;
   try {
     userName = await clientWelcome();
-    // await declareAndBind(
-    //   conn, 
-    //   ExchangePerilDirect, 
-    //   `${PauseKey}.${userName}`, 
-    //   PauseKey, 
-    //   SimpleQueueType.Transient
-    // );
 
     // create gamestate
     const gameState = new GameState(userName);
+    const publishCh = await conn.createConfirmChannel();
 
     await subscribeJSON(conn, ExchangePerilDirect, `${PauseKey}.${userName}`, PauseKey, SimpleQueueType.Transient, handlerPause(gameState));
+    await subscribeJSON(conn, ExchangePerilTopic, `${ArmyMovesPrefix}.${userName}`, `${ArmyMovesPrefix}.*`, SimpleQueueType.Transient, handlerMove(gameState));
+
 
     while (true) {
       const input: string[] = await getInput();
@@ -39,7 +36,8 @@ async function main() {
         if (input[0] === 'spawn') {
           commandSpawn(gameState, input);
         } else if (input[0] === 'move') {
-          commandMove(gameState, input);
+          const move = commandMove(gameState, input);
+          publishJSON(publishCh, ExchangePerilTopic, `${ArmyMovesPrefix}.${userName}`, move);
         } else if (input[0] === 'stats') {
           commandStatus(gameState);
         } else if (input[0] === 'help') {
